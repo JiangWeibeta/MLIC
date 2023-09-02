6aec1146d7f262a9847a01afd5a444c3242e8670
JiangWeibeta <wei.jiang1999@outlook.com>
2023-09-02 16:35:05 +0800

    clean command

--- playground/train.sh
+++ playground/train.sh
@@ -1,3 +1,3 @@
 work_path=$(dirname $0)
 export PYTHONPATH=..:$PYTHONPATH
-nohup python train.py --metrics mse --exp slic_ablation_channel_0035_7777_192v2 -c /data2/jiangwei/work_space/CCSLICfinal/playground/experiments/slic_ablation_channel_0035_7777_192v2/checkpoints/checkpoint_025.pth.tar --gpu_id 1 --lambda 0.0035 -lr 1e-4 --clip_max_norm 1.0 --seed 1984 --batch-size 8 & > 0035v2.txt
+nohup python train.py --metrics mse --exp mlicpp_mse_q1 -c /data2/jiangwei/work_space/MLICPP/playground/experiments/mlicpp_mse_q1/checkpoints/checkpoint_025.pth.tar --gpu_id 1 --lambda 0.0018 -lr 1e-4 --clip_max_norm 1.0 --seed 1984 --batch-size 32 & > 0035v2.txt
--- playground/train_msssim.sh
+++ playground/train_msssim.sh
@@ -1,3 +1,3 @@
 work_path=$(dirname $0)
 export PYTHONPATH=..:$PYTHONPATH
-nohup python train.py --metrics ms-ssim --exp slic_uneven_charm_ms_0873_192 --gpu_id 1 --lambda 8.73 -lr 1e-4 --clip_max_norm 1.0 --seed 909 --batch-size 8 -c /data2/jiangwei/work_space/CCSLICfinal/playground/experiments/slic_uneven_charn_ms_6050_192/checkpoint_0067.pth.tar & > 0130v2.txt
+nohup python train.py --metrics ms-ssim --exp mlicpp_msssim_q1 --gpu_id 1 --lambda 2.4 -lr 1e-4 --clip_max_norm 1.0 --seed 909 --batch-size 8 -c /data2/jiangwei/work_space/MLICPP/playground/experiments/mlicpp_msssim_q1/checkpoint_0067.pth.tar & > 0130v2.txt

a05aa5ab0f539466c62b80c5dbf2b2d7ec8acc09
Jiang Wei <55226087+JiangWeibeta@users.noreply.github.com>
2024-09-02 11:00:53 +0800

    Update context.py

--- MLIC++/modules/transform/context.py
+++ MLIC++/modules/transform/context.py
@@ -134,29 +134,6 @@ class ChannelContext(nn.Module):
 
         return channel_params
 
-
-class ChannelContextEX(nn.Module):
-    def __init__(self, in_dim, out_dim, act=nn.GELU) -> None:
-        super().__init__()
-        self.fushion = nn.Sequential(
-            nn.Conv2d(in_dim, 224, kernel_size=3, stride=1, padding=1),
-            act(),
-            nn.Conv2d(224, 128, kernel_size=3, stride=1, padding=1),
-            act(),
-            nn.Conv2d(128, out_dim, kernel_size=3, stride=1, padding=1)
-        )
-
-    def forward(self, channel_params):
-        """
-        Args:
-            channel_params(Tensor): [B, C * K, H, W]
-        return:
-            channel_params(Tensor): [B, C * 2, H, W]
-        """
-        channel_params = self.fushion(channel_params)
-
-        return channel_params
-
 class LinearGlobalIntraContext(nn.Module):
     def __init__(
             self,
